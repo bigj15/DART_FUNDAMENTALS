cb5ea713f9e0eb4fa5fc95d6fbd12df0e1c28349
bigj15 <bigjee15@gmail.com>
2024-11-09 20:49:34 +0000

    first commit

---
+++ test1.dart
@@ -0,0 +1,25 @@
+void main(){
+
+  int x = 34;
+  print(x);
+  
+  String firstName = "Jedel";
+  print("String: $firstName");
+
+  int myNum = 33;
+  print("Integer: $myNum");
+  double otherNum = 22.34;
+  print("Double: $otherNum");
+  bool myBool = true;
+  print("Boolean: $myBool");
+
+  dynamic fullName = "Jedel Annan";
+  print("Dynamic: $fullName");
+
+
+
+
+}
+
+
+
