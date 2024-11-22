a052816efd224081b112a30fcb87859b33304966
bigj15 <bigjee15@gmail.com>
2024-11-22 16:34:13 +0000

    loops in dart

---
+++ Loops/dartLoops.dart
@@ -0,0 +1,23 @@
+void main(){
+//For loop
+int num = 5;
+for(int i = num;i >= 1; i--){
+  print(i);
+}
+
+//For In loop
+var names = ["Peter","James","John"];
+for(var name in names){
+  print(name);
+}
+
+//While loop
+while(num >= 1){
+  print(num);
+  num--;
+}
+
+
+
+
+}
