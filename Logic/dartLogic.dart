7c46cd65e9702278934ea101167c835192c369fb
bigj15 <bigjee15@gmail.com>
2024-11-28 19:27:52 +0000

    feat:logic

---
+++ Logic/dartLogic.dart
@@ -0,0 +1,19 @@
+void main (){
+    // Logic Statements: If, If/Else, If Else If
+     var num = 6;
+     if(num == 5){
+      print("The number is 5!"); 
+    }
+
+     else if(num == 3){
+      print("The number is 3!");    
+    }
+
+     else{
+      print("The number is neither 5 nor 3, it is $num.");
+    }
+
+
+
+
+}
